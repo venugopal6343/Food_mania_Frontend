8fa67c1bcb6d85af15bb30585fbf3ecbcf4fc08d
venugopal6343 <venugopalreddy915@gmail.com>
2026-04-28 20:18:10 +0530

    adding railway url in apibase url

--- src/app/configs/environment.prod.ts
+++ src/app/configs/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   appName: 'Food Mania',
-  apiBaseUrl: '',
+  apiBaseUrl: 'https://foodmaniabackend-production.up.railway.app/api/v1',
   defaultCurrency: 'INR',
   authStorageKey: 'food_mania_auth',
   requestTimeoutMs: 30000

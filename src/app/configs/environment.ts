0d1a563f42597e7368937f8e80f00819290c5697
venugopal6343 <venugopalreddy915@gmail.com>
2026-05-16 13:54:26 +0530

    port changes to 8080 from 8081

--- src/app/configs/environment.ts
+++ src/app/configs/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   appName: 'Food Mania',
-  apiBaseUrl: 'http://localhost:8081',
+  apiBaseUrl: 'http://localhost:8080',
   defaultCurrency: 'INR',
   authStorageKey: 'food_mania_auth',
   requestTimeoutMs: 30000
